0a755b1d582eaf6d4f585374e8ac2ce7e2a16cc5
SaiAkhilP <saiakhil_p@yahoo.com>
2020-02-17 17:34:08 +0530

    First Commit

---
+++ app.js
@@ -0,0 +1,68 @@
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+const session = require('express-session');
+const cors = require('cors');
+const mongoose = require('mongoose');
+const errorHandler = require('errorhandler');
+const morgan = require('morgan')
+// const mongodb=require('mongodb')
+
+//Configure mongoose's promise to global promise
+mongoose.promise = global.Promise;
+//Configure isProduction variable
+const isProduction = process.env.NODE_ENV === 'production';
+
+//Initiate our app
+const app = express();
+
+//Configure our app
+app.use(cors());
+app.use(require('morgan')('dev'));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(session({ secret: 'passport-tutorial', cookie: { maxAge: 60000 }, resave: false, saveUninitialized: false }));
+
+if(!isProduction) {
+    app.use(errorHandler());
+  }
+
+//Configure Mongoose
+
+
+mongoose.connect('mongodb+srv://cluster0-oq9o6.mongodb.net/test  --username sai6019');
+mongoose.set('debug', true);
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+    console.log("Connection Successful!");
+});
+
+//Error handlers & middlewares
+if(!isProduction) {
+    app.use((err, req, res) => {
+      res.status(err.status || 500);
+  
+      res.json({
+        errors: {
+          message: err.message,
+          error: err,
+        },
+      });
+    });
+  }
+
+  
+app.use((err, req, res) => {
+    res.status(err.status || 500);
+  
+    res.json({
+      errors: {
+        message: err.message,
+        error: {},
+      },
+    });
+  });
+  
+  app.listen(8000, () => console.log('Server running on http://localhost:8000/'));
